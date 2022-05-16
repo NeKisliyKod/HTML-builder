053cef1e6a4f046bb0969143759661ff4d5c984c
Vasiliy <>
2022-05-16 21:00:27 +0300

    feat: 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+let filePath = path.join(__dirname, 'text.txt');
+const readStream = fs.createReadStream(filePath);
+
+
+readStream.on('data', (chunk) => {
+  console.log(chunk.toString());
+});
